3cd2aab7759becf94abb3bb9dbb28d999e8523e9
PiotrSwietek <piotr.swietek95@gmail.com>
2018-05-05 16:17:05 +0200

    A* first commit

--- RushHour/src/AStar.java
+++ RushHour/src/AStar.java
@@ -1,5 +1,4 @@
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * This is the template for a class that performs A* search on a given
@@ -15,14 +14,69 @@ import java.util.Queue;
 public class AStar {
 
     /** The solution path is stored here */
-    public State[] path;
+    public List<State> path;
+    private Puzzle puzzle;
+    private Heuristic heuristic;
 
+    //init the openQueue, initialCapacity set to 11 as specified in the docs
+    final Queue<Node> openQueue = new PriorityQueue<>(11, new NodeComparator());
+
+    //initialize closedList
+    final Set<Node> closedList = new HashSet<>();
     /**
      * This is the constructor that performs A* search to compute a
      * solution for the given puzzle using the given heuristic.
      */
     public AStar(Puzzle puzzle, Heuristic heuristic) {
+        this.puzzle = puzzle;
+        this.heuristic = heuristic;
+    }
 
+    public List<State> astar(){
+        //init the result Path
+        List<State> resultPath = new ArrayList<>();
+
+        //get the first node
+        Node startNode = puzzle.getInitNode();
+
+        // add node to openQueue
+        openQueue.add(startNode);
+
+
+        while(!openQueue.isEmpty()){
+            //get and remove node from openQueue
+            final Node currentNode = openQueue.poll();
+
+            //is this the goal? if yes return the resultpath
+            if(currentNode.getState().isGoal()){
+                return resultPath;
+            }
+
+            //add this node to closedList
+            closedList.add(currentNode);
+
+            //expand the current open node and add its children to open list
+            expandNode(currentNode);
+        }
+
+        return null;
+    }
+
+    /*expands given node and adds its children to openQueue IF:
+    *  - succesor found for the first time
+    *  - a better path to the node is found
+    * */
+    private void expandNode(Node node){
+        List<Node> childNodes = Arrays.asList(node.expand());
+
+        for (Node child : childNodes) {
+            if (!closedList.contains(child)){
+
+                int tentative_g = 0; //what is this?
+
+            }
+
+        }
     }
 
 }
---
+++ RushHour/src/NodeComparator.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class NodeComparator implements Comparator<Node> {
+    public int compare(Node nodeFirst, Node nodeSecond) {
+
+        return 0;
+    }
+}
